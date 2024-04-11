e35a51868198e192e24b44ebf57282766c025231
Jacob Floyd <41782145+JacobFloydTech@users.noreply.github.com>
2024-04-11 23:06:40 +1200

    modified eslint rules

--- app/exampleWebsites.tsx
+++ app/exampleWebsites.tsx
@@ -14,17 +14,14 @@ export default function ExampleWebsites() {
 }
 
 function RisosEnterprisesWebsite() { 
-    const handleOver = () => {
-        document.getElementById('risosCover')?.classList.add('animate');
-    } 
-    
+    const handleOver = () =>  document.getElementById('risosCover')?.classList.add('animate');
     const handleLeave = () => document.getElementById('risosCover')?.classList.remove('animate');
     return (
-        <div onMouseEnter={() => handleOver()} onMouseLeave={() => handleLeave()} className="website p-2 backdrop-blur-xl rounded-3xl relative transition-all duration-150 hover:scale-[102%] hover:-translate-y-2 overflow-hidden">
+        <button onClick={() => window.location.href = 'http://www.risosenterprises.com'} onMouseEnter={() => handleOver()} onMouseLeave={() => handleLeave()} className="website p-2 backdrop-blur-xl rounded-3xl relative transition-all duration-150 hover:scale-[102%] hover:-translate-y-2 overflow-hidden">
             <img className="rounded-3xl" src="risosenterprisessnapshot.jpeg" />
             <GrainFilter/>
             <RisosHoverCover/>
-        </div>
+        </button>
     )
     function RisosHoverCover() { 
         return ( 
@@ -37,10 +34,7 @@ function RisosEnterprisesWebsite() {
 }
 
 function CarMarket() { 
-    const handleOver = () => {
-        document.getElementById('carCover')?.classList.add('animate');
-    } 
-    
+    const handleOver = () => document.getElementById('carCover')?.classList.add('animate');
     const handleLeave = () => document.getElementById('carCover')?.classList.remove('animate');
     
     return ( 
@@ -53,7 +47,7 @@ function CarMarket() {
     function CarkMarketCover() { 
         return ( 
             <div id="carCover" className="bg-[rgba(0,0,0,0.5)] flex flex-col justify-center items-center w-full h-full absolute top-0 translatexfull left-0 z-50">
-                <p className="text-white text-2xl text-center py-4 font-bold">CarMarket.com</p>
+                <p className="text-white text-2xl text-center py-4 font-bold">MovieDatabases</p>
                 <p className="text-white text-2xl text-center py-4 font-bold">A mockup site built using SvelteKit, Tailwind, Express, Puppeteer, Firebase, and AWS</p>
             </div>
         )
