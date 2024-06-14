34191c962c084b0f690faf5fb24799b2376fd3a2
Jacob Floyd <41782145+JacobFloydTech@users.noreply.github.com>
2024-06-14 14:02:41 +1200

    updated grid

--- app/background.tsx
+++ app/background.tsx
@@ -11,6 +11,7 @@ export default function Background() {
                 requestAnimationFrame(updateAngle);
         };
         updateAngle();
+        addDelays();
         window.addEventListener('resize', setUpGrid)
         return () => window.removeEventListener('resize', setUpGrid)
  
@@ -27,13 +28,23 @@ export default function Background() {
             for (var j = 0; j < row; j++) {
                 const squareElemeent = document.createElement('div');
                 squareElemeent.classList.add('square');
-                const delay = Math.sqrt((i * j) / 5) / 2.5;
-                squareElemeent.style.animationDelay = `${delay}s`;
-                squareElemeent.id = `${i},${j}`
                 el.appendChild(squareElemeent);
             }
         }
-
+        addDelays();
+    }
+    const addDelays = () => { 
+        const element = document.getElementById('customGrid');
+        if (!element) { return}
+        const cols = getComputedStyle(element).gridTemplateColumns.split(" ").length;
+        const children = Array.from(element.children);
+        for (var i = 0; i < children.length; i++) { 
+            const x = i % cols;
+            const y = Math.floor(i / cols);
+            const delay = Math.sqrt((x*y) / 5) / 2.5;
+            (children[i] as HTMLElement).style.animationDelay = `${delay}s`
+            children[i].setAttribute('id', `${x},${y}`)
+        } 
     }
 
 
@@ -42,7 +53,7 @@ export default function Background() {
         <div id='backgroundContainer' className="overflow-x-hidden relative h-full w-screen">
             <div className=" w-full absolute background h-full -z-20" />
             <div className="fadeBackground z-50"/>
-            <div id='customGrid' className="customGrid overflow-hidden w-[120vw] md:w-[110vw] lg:w-[105vw] xl:w-[101vw] 3xl:w-[100vw] top-0 -translate-y-1  p-0 m-0 mx-auto"/>
+            <div id='customGrid' className="customGrid overflow-hidden w-[120vw] md:w-[110vw] lg:w-[105vw] xl:w-[105vw]  top-0 -translate-y-1  p-0 m-0 mx-auto"/>
         
             
         </div>
